939d7bb7f08dc7d3506980f10850b9a74f64a643
Shivani Bagal <67872906+shivaniB8@users.noreply.github.com>
2022-05-17 18:27:16 +0530

    create users listing (#1)
    * create users listing
    
    * Users listing
    
    - fetch users data from API
    - create user model
    
    * Refactor code
    
    - user_service file moved from page to services directory

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'package:gorestapp/services/user_service.dart';
+import 'package:gorestapp/page/user.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<User> _usersList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getUsers();
+  }
+
+  void _getUsers() async {
+    _usersList = await getUsers();
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: ListView.separated(
+          separatorBuilder: (BuildContext context, int index) => Divider(
+            color: Colors.blueGrey,
+          ),
+          itemCount: _usersList.length,
+          itemBuilder: (BuildContext context, int index) {
+            var user = _usersList[index];
+            return ListTile(
+              title: Text(user.name ?? ""),
+              subtitle: Text(user.email ?? ""),
+              leading: CircleAvatar(child: Text('A')),
+              trailing: Icon(Icons.favorite_border),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
