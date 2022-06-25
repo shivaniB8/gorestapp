ddaac59b9221bca36b185f8888a31bd10b890290
shivaniB8 <sdbagal@mitaoe.ac.in>
2022-06-25 20:33:45 +0530

    Shimmer effect

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gorestapp/extension/string_extensions.dart';
+import 'package:gorestapp/page/skeleton.dart';
 import 'package:gorestapp/screens/add.dart';
 import 'package:gorestapp/services/user_service.dart';
 import 'package:gorestapp/page/user.dart';
@@ -13,9 +14,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<User> _usersList = [];
+  late bool _isLoading;
 
   @override
   void initState() {
+    _isLoading = true;
     super.initState();
     _getUsers();
   }
@@ -23,6 +26,7 @@ class _HomePageState extends State<HomePage> {
   void _getUsers() async {
     _usersList = await getUsers();
     setState(() {});
+    _isLoading=false;
   }
 
   @override
@@ -32,8 +36,15 @@ class _HomePageState extends State<HomePage> {
         title: Text('Dashboard'),
       ),
       body: Container(
-        padding: EdgeInsets.all(20),
-        child: ListView.separated(
+        child: _isLoading
+            ? ListView.separated(
+          separatorBuilder: (BuildContext context, int index) => Divider(
+            color: Colors.blueGrey[200],
+          ),
+          itemCount: 10,
+          itemBuilder: (context, index) => const NewsCardSkelton(),
+        )
+            : ListView.separated(
           separatorBuilder: (BuildContext context, int index) => Divider(
             color: Colors.blueGrey,
           ),
@@ -73,5 +84,46 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+class NewsCardSkelton extends StatelessWidget {
+  const NewsCardSkelton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Skeleton(height: 60, width: 60),
+        const SizedBox(
+          width: 16,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Skeleton(width: 200),
+              const SizedBox(height: 4),
+              const Skeleton(),
+              const SizedBox(height: 8),
+              Row(
+                children: const [
+                  Expanded(
+                    child: Skeleton(),
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: Skeleton(),
+                  ),
+                ],
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
 
 
