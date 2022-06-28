834211ea4523ec60d22ad1438ff41cba5c1aa193
shivaniB8 <sdbagal@mitaoe.ac.in>
2022-06-29 00:03:09 +0530

    Create user
     -create new user by sending data to api
     -fetching that new user in list

--- lib/screens/add.dart
+++ lib/screens/add.dart
@@ -1,162 +1,124 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:gorestapp/page/post_user.dart';
+import 'package:gorestapp/services/user_service.dart';
 
 class AddUserPage extends StatefulWidget {
   @override
-  _AddUserPageState createState() => _AddUserPageState();
+  State<AddUserPage> createState() => _AddUserPageState();
 }
 
 class _AddUserPageState extends State<AddUserPage> {
-  final _gender = ['Male', 'Female', 'Other'];
+  final _gender = ['Male', 'Female'];
   final _status = ['Active', 'Inactive'];
   String? _statusValue;
   String? _genderValue;
-  GlobalKey<FormState> formkey = GlobalKey<FormState>();
-  void validate() {
-    if (formkey.currentState!.validate()) {
-      print("Validated");
-    } else {
-      print("Not Validated");
-    }
-  }
-
-
-  String? isRequiredValidator(value) {
-    if (value.isEmpty) {
-      return "required";
-    } else {
-      return null;
-    }
-  }
+  late Post _post;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
 
   DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
-    value: '$item',
-    child: Text(
-      '$item',
-      style: const TextStyle(fontSize: 15),
-    ),
-  );
+        value: '$item',
+        child: Text(
+          '$item',
+          style: const TextStyle(fontSize: 15),
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Add New User"),
-        backgroundColor: Colors.green,
+        title: Text('Add New User'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(25.0),
-        child: Center(
-          child: Form(
-            key: formkey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'User Form',
-                  style: const TextStyle(
-                      fontWeight: FontWeight.w200,
-                      fontSize: 35,
-                      color: Colors.green),
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Name",
-                      prefixIcon: Align(
-                        widthFactor: 1.0,
-                        heightFactor: 1.0,
-                        child: Icon(
-                          Icons.person,
-                        ),
-                      ),
-                    ),
-                    validator: isRequiredValidator,
-                ),
-                SizedBox(height:16),
-                TextFormField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Email",
-                      prefixIcon: Align(
-                        widthFactor: 1.0,
-                        heightFactor: 1.0,
-                        child: Icon(
-                          Icons.person,
-                        ),
-                      ),
-                    ),
-                    validator: isRequiredValidator,
-                ),
-                SizedBox(height:16),
-                Center(
-                  child: Container(
-                    padding:
-                    EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-                    width: double.infinity,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black38),
-                    ),
-                    child: DropdownButtonHideUnderline(
-                      child: DropdownButton<String>(
-                        hint: Text('Select Your Gender'),
-                        value: _genderValue,
-                        isExpanded: true,
-                        items: _gender.map(buildMenuItem).toList(),
-                        onChanged: (value) =>
-                            setState(() => this._genderValue = value),
-                      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Enter name'),
+                controller: nameController,
+              ),
+              SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Enter email'),
+                controller: emailController,
+              ),
+              SizedBox(height: 16),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                  width: double.infinity,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black38),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      hint: Text('Select Your Gender'),
+                      value: _genderValue,
+                      isExpanded: true,
+                      items: _gender.map(buildMenuItem).toList(),
+                      onChanged: (value) =>
+                          setState(() => this._genderValue = value),
                     ),
                   ),
                 ),
-                SizedBox(
-                  height: 16,
-                ),
-                Center(
-                  child: Container(
-                    padding:
-                    EdgeInsets.symmetric(horizontal: 10, vertical: 2),
-                    width: double.infinity,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black38),
-                    ),
-                    child: DropdownButtonHideUnderline(
-                      child: DropdownButton<String>(
-                        hint: Text('Select your Status'),
-                        value: _statusValue,
-                        isExpanded: true,
-                        items: _status.map(buildMenuItem).toList(),
-                        onChanged: (value) =>
-                            setState(() => this._statusValue = value),
-                      ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Center(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                  width: double.infinity,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black38),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      hint: Text('Select your Status'),
+                      value: _statusValue,
+                      isExpanded: true,
+                      items: _status.map(buildMenuItem).toList(),
+                      onChanged: (value) =>
+                          setState(() => this._statusValue = value),
                     ),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 20.0,
-                  ),
-                  child:SizedBox(
-                    width: 200.0,
-                    height: 50.0,
-                    child: ElevatedButton(
-                      onPressed: validate,
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 20.0,
+                ),
+                child: SizedBox(
+                  width: 200.0,
+                  height: 50.0,
+                  child: ElevatedButton(
+                      onPressed: () async {
+                        String name = nameController.text;
+                        String email = emailController.text;
+                        String gender = _genderValue.toString();
+                        String status = _statusValue.toString();
+                        Post? data =
+                            await submitData(name, email, gender, status);
+                        setState(() {
+                          _post = data!;
+                        });
+                      },
                       child: Text(
-                        "Submit",
+                        'Submit',
                         style: const TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ),
+                      )),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
-
 }
--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:gorestapp/page/post_user.dart';
 import 'package:gorestapp/page/user.dart';
 import 'package:http/http.dart' as http;
 
@@ -7,4 +8,19 @@ Future<List<User>> getUsers() async {
       await http.get(Uri.parse('https://gorest.co.in/public/v2/users'));
   var json = jsonDecode(response.body);
   return json.map<User>((user) => User.fromJson(user)).toList();
+}
+
+Future<Post?> submitData(
+    String name, String email, String gender, String status) async {
+  var response = await http.post(
+      Uri.parse('https://gorest.co.in/public/v2/users'),
+      headers: {'Authorization':'Bearer eddd3b2a93a235976eabb880d1c14ca131c4c3ff32805777a61ecfc8c6c1bc3f'},
+      body: {'name': name, 'email': email, 'gender': gender, 'status': status,});
+  var data = response.body;
+  print(data);
+  if (response.statusCode == 201) {
+    String responseString = response.body;
+    postFromJson(responseString);
+  } else
+    return null;
 }
